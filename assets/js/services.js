425db7f010a94870d68f6c78b568ffb03ecb9d1c
Alazar-dev <mealazartadesse@gmail.com>
2021-01-08 11:42:34 +0300

    changed previosly haded images and added functionality through service.js

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,3 +1,11 @@
 $(document).ready(()=>{
-    $("#footer").css('hide');
+    $("#corporateCarImage").click(function(){
+        $('#corporateCarImages, #corporate').removeClass('hiddenCorporateCarImages');
+    });
+    $("#midSizeCarImage").click(function() {
+        $("#midSizeCarImages, #mid-size").removeClass('hiddenMidSizeCarImages');
+    });
+    $("#englishSpeakerCarImage").click(function(){
+        $("#englishSpeakerCarImages, #english-speaker").removeClass('hiddenEnglishSpeakerCarImages');
+    });
 })
